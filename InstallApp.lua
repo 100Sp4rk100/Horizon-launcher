011cfa0538642049bfffe4a7268f5aa00b7d755f
100Sp4rk100 <168758024+100Sp4rk100@users.noreply.github.com>
2025-05-07 12:15:20 +0200

    Delete InstallApp.lua

--- InstallApp.lua
+++
@@ -1,9 +0,0 @@
-dofile("os/File System Management/Data System Management/Data System Management.lua")
-
-print("Enter app name : ")
-local name = io.read()
-
-print("Enter app id : ")
-local id = io.read()
-
-addSystemData("ApplicationList", {name..":"..id}, getSystemKey("ApplicationList"))
